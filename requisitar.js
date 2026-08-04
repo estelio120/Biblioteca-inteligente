ea60a08e32d3790e0d47aa77753e45adedc0c6fb
estelio120 <steliobahate190@gmail.com>
2026-08-04 16:48:13 +0200

    Refactor requisitar.js for improved readability

--- requisitar.js
+++ requisitar.js
@@ -242,14 +242,7 @@ function mostrarConfirmacao() {
     };
 
 }
-const menu=document.querySelector(".menu");
-const btn=document.getElementById("menuBtn");
-const overlay=document.getElementById("overlay");
 
-btn.addEventListener("click",()=>{
-menu.classList.toggle("active");
-overlay.classList.toggle("active");
-});
 
 overlay.addEventListener("click",()=>{
 menu.classList.remove("active");
